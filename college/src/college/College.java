1e3df315589fc854ce58d55ad4c6d4f1913647a7
hxrrybarry <145754861+hxrrybarry@users.noreply.github.com>
2023-09-22 09:51:23 +0100

    implemented british slang

---
+++ college/src/college/College.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package college;
+
+/**
+ *
+ * @author harib
+ */
+public class College 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("ey up cocker");
+    }
+    
+}
